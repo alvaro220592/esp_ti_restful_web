02e238734e72727c4fc215f15fc0683c82d753fa
alvaro220592 <alvaro220592@gmail.com>
2021-10-04 21:56:37 -0300

    versionamento de api

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\v1\ProductController;
 
-//Route::get('products', [ProductController::class, 'index']);
-Route::resource('products', ProductController::class);
+Route::group(['prefix' => 'v1'], function(){
+    Route::resource('products', ProductController::class);
+});
 
-//Route::post('product/create', [ProductController::class, 'store']);
+Route::group(['prefix' => 'v2'], function(){
+    Route::resource('products', ProductController::class);
+});
